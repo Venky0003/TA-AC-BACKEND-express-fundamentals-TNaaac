694f2250077cb67d47a6dcb2afd2c04a7a28be96
Venky0003 <rajv17270@gmail.com>
2023-04-18 12:24:30 +0530

    add single get

--- block-BNaabk/sample/app.js
+++ block-BNaabk/sample/app.js
@@ -5,9 +5,7 @@ let app = express();
 app.get('/', (req, res) => {
   res.send('Welcome');
 });
-app.get('/about', (req, res) => {
-  res.send('About page');
-});
+
 app.listen(3000, () => {
   console.log('Server Listening on Port 3k');
 });
